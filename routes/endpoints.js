6015dbe3c4a6f2e3cb96fc3b5256eae2db82d288
Shubham Aggarwal <sbmaggarwal@gmail.com>
2023-03-15 21:13:35 +0530

    Added support to check if queue exists and create fifio queue

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.post('/message', express.json(), async function (req, res) {
     try {
 
-        const {billNumber, paymentId} = req.body;
+        const {groupId, uniqueIdentifier} = req.body;
         const message = JSON.stringify(req.body);
         console.log(`Body: '${JSON.stringify(req.body, null, '\t')}'.`);
         const params = {
             MessageBody: message,
-            MessageDeduplicationId: String(paymentId),  // Required for FIFO queues
-            MessageGroupId: billNumber,  // Required for FIFO queues
+            MessageDeduplicationId: String(uniqueIdentifier),  // Required for FIFO queues
+            MessageGroupId: groupId,  // Required for FIFO queues
             QueueUrl: process.env.QUEUE_URL
         };
 
@@ -24,6 +24,49 @@ router.post('/message', express.json(), async function (req, res) {
                 console.log("Success", data.MessageId);
                 res.status(200).send({message: 'Message Sent', success: true});
             }
+        });
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
+
+router.post('/check', express.json(), async function (req, res) {
+    try {
+
+        const {queueName} = req.body;
+
+        const params = {
+            QueueName: queueName
+        };
+
+        sqs.getQueueUrl(params, function (err, data) {
+            if (err) res.status(500).send(err); // an error occurred
+            else res.status(200).send(data);           // successful response
+        });
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+});
+
+router.post('/create', express.json(), async function (req, res) {
+    try {
+
+        const {queueName} = req.body;
+
+        const params = {
+            QueueName: queueName,
+            Attributes: {
+                'FifoQueue': 'true',
+                'DeduplicationScope': 'messageGroup',
+                'SqsManagedSseEnabled': 'false',
+            }
+        };
+
+        sqs.createQueue(params, function (err, data) {
+            if (err) res.status(500).send(err); // an error occurred
+            else res.status(200).send(data);           // successful response
         });
 
     } catch (err) {
